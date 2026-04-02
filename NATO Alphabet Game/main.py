8f38e6341cc67c9b8ef38554d2eccd2cb9419a27
cyndizzz <yuxinzhong7@hotmail.com>
2026-04-02 15:32:58 -0400

    update NATO Phonetic Game

--- NATO Alphabet Game/main.py
+++ NATO Alphabet Game/main.py
@@ -6,7 +6,6 @@ df = pd.read_csv('nato_phonetic_alphabet.csv')
 dict = { row.letter:row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name: ")
-list_input = list(user_input.upper())
-output_list = [ value  for (key,value) in dict.items() if key in list_input ]
+user_input = input("Enter your name: ").upper()
+output_list = [ dict[letter] for letter in user_input ]
 print(output_list)
